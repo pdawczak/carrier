21a329e519d1b4e2d800f8bef7cc6ccc0200ad01
Paweł Dawczak <pawel.dawczak@gmail.com>
2012-12-13 20:59:09 +0100

    Entity and Repository for Deliveries created

---
+++ src/CRR/Bundle/DeliveryBundle/Entity/Delivery.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace CRR\Bundle\DeliveryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use CRR\Bundle\PersonBundle\Entity\Person;
+
+/**
+ * Delivery
+ *
+ * @ORM\Table(name="deliveries")
+ * @ORM\Entity(repositoryClass="CRR\Bundle\DeliveryBundle\Entity\DeliveryRepository")
+ */
+class Delivery
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var Person $sender
+     *
+     * @ORM\ManyToOne(targetEntity="CRR\Bundle\PersonBundle\Entity\Person")
+     */
+    protected $sender;
+
+    /**
+     * @var Person $receiver
+     *
+     * @ORM\ManyToOne(targetEntity="CRR\Bundle\PersonBundle\Entity\Person")
+     */
+    protected $receiver;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set sender
+     *
+     * @param \CRR\Bundle\PersonBundle\Entity\Person $sender
+     * @return Delivery
+     */
+    public function setSender(Person $sender = null)
+    {
+        $this->sender = $sender;
+    
+        return $this;
+    }
+
+    /**
+     * Get sender
+     *
+     * @return \CRR\Bundle\PersonBundle\Entity\Person 
+     */
+    public function getSender()
+    {
+        return $this->sender;
+    }
+
+    /**
+     * Set receiver
+     *
+     * @param \CRR\Bundle\PersonBundle\Entity\Person $receiver
+     * @return Delivery
+     */
+    public function setReceiver(Person $receiver = null)
+    {
+        $this->receiver = $receiver;
+    
+        return $this;
+    }
+
+    /**
+     * Get receiver
+     *
+     * @return \CRR\Bundle\PersonBundle\Entity\Person 
+     */
+    public function getReceiver()
+    {
+        return $this->receiver;
+    }
+}
